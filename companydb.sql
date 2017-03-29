4fa8a46b5a8c758d77f026d38ebdb1eb74ce2287
Artemii Yanushevskyi <argoniton@gmail.com>
2017-03-29 16:33:43 +0300

    exported processed db

--- companydb.sql
+++ companydb.sql
@@ -1,15 +1,42 @@
 -- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2017 at 01:26 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `companydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
 
 CREATE TABLE `employee` (
   `Name` varchar(50) NOT NULL,
   `Position` varchar(50) NOT NULL,
   `Date` date NOT NULL,
   `Experience` varchar(100) NOT NULL,
-  `Education` varchar(100) NOT NULL,
-  FOREIGN KEY (Name) REFERENCES product(Author),
-  FOREIGN KEY (Position) REFERENCES position(Name)
+  `Education` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `employee`
+--
 
 INSERT INTO `employee` (`Name`, `Position`, `Date`, `Experience`, `Education`) VALUES
 ('Artemii', 'Founder', '0000-00-00', '', 'KNU MechMat'),
@@ -18,6 +45,11 @@ INSERT INTO `employee` (`Name`, `Position`, `Date`, `Experience`, `Education`) V
 ('Ivan', 'Editor-at-large', '0000-00-00', '', 'KNU MechMat'),
 ('Vova', 'Editor-in-chief', '0000-00-00', '', 'KNU MechMat');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `position`
+--
 
 CREATE TABLE `position` (
   `Name` varchar(50) NOT NULL,
@@ -29,6 +61,9 @@ CREATE TABLE `position` (
   `Salary` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `position`
+--
 
 INSERT INTO `position` (`Name`, `Primary responsibility`, `Secondary responsibility`, `Clock in`, `Clock out`, `Department`, `Salary`) VALUES
 ('CEO', 'Management', 'PR', '09:00:00.000000', '06:00:00.000000', 'Main', NULL),
@@ -37,6 +72,11 @@ INSERT INTO `position` (`Name`, `Primary responsibility`, `Secondary responsibil
 ('Event Organiser', 'Organize events', 'Presentation', '09:00:00.000000', '02:00:00.000000', 'Conference hall', NULL),
 ('Founder', 'Management', 'Directional decisions', '09:00:00.000000', '06:00:00.000000', 'Main', NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
 
 CREATE TABLE `product` (
   `Type` varchar(50) DEFAULT NULL,
@@ -47,16 +87,21 @@ CREATE TABLE `product` (
   `Add revenues` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#
+--
+-- Dumping data for table `product`
+--
 
 INSERT INTO `product` (`Type`, `Format`, `Name`, `Author`, `Date`, `Add revenues`) VALUES
-('Article', 'Top 10', '\"Best Movies in 2016\"', 'Ivan', '0000-00-00', '170.00'),
-('Podcast', 'How to', '\"Prepare dinner for evening\"', 'Artemii', '0000-00-00', '340.00'),
-('Article', 'Research', '\"Overwhelming Political Pressure\"', 'Ivan', '0000-00-00', '500.00'),
-('Video', 'Education', '\"German Culture\"', 'Carolina', '0000-00-00', '667.00'),
-('Podcast', 'Entertainment', '\"Game review\"', 'Vova', '0000-00-00', '832.00'),
-('Event', 'Lecture', '\"Success stories\"', 'Ira', '0000-00-00', '997.00'),
-('Video', 'Research', '\"Overview of US politics\"', 'Artemii', '0000-00-00', '1162.00'),
-('Podcast', 'Top 10', '\"Top 100 rich people in the world\"', 'Carolina', '0000-00-00', '1327.00'),
-('Article', 'Entertainment', '\"Weekend family leizure\"', 'Vova', '0000-00-00', '1492.00');
+('Article', 'Top 10', '"Best Movies in 2016"', 'Ivan', '0000-00-00', '170.00'),
+('Podcast', 'How to', '"Prepare dinner for evening"', 'Artemii', '0000-00-00', '340.00'),
+('Article', 'Research', '"Overwhelming Political Pressure"', 'Ivan', '0000-00-00', '500.00'),
+('Video', 'Education', '"German Culture"', 'Carolina', '0000-00-00', '667.00'),
+('Podcast', 'Entertainment', '"Game review"', 'Vova', '0000-00-00', '832.00'),
+('Event', 'Lecture', '"Success stories"', 'Ira', '0000-00-00', '997.00'),
+('Video', 'Research', '"Overview of US politics"', 'Artemii', '0000-00-00', '1162.00'),
+('Podcast', 'Top 10', '"Top 100 rich people in the world"', 'Carolina', '0000-00-00', '1327.00'),
+('Article', 'Entertainment', '"Weekend family leizure"', 'Vova', '0000-00-00', '1492.00');
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
